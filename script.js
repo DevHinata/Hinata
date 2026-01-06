b1544c8f309c6003674c5bbc3038eece1a9eb497
一 Hɩňʌtʌ ᯓᡣ𐭩 <abhishekmondol378@gmail.com>
2026-01-06 10:11:05 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Auto year in footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Future animations and interactions can go here
+console.log("Portfolio loaded.");
